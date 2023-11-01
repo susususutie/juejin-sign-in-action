46b163d3f7ca9b2231a8ffee7a9716f07da306d5
sutie <sususu_sutie@qq.com>
2023-11-01 15:57:57 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -1,35 +1,67 @@
 import os
-
 import requests
+import json
+import random
 
 # 添加 server 酱通知
-server_key = os.environ["SERVER_KEY"]
+server_key = os.environ.get("SERVER_KEY")
 
 # 获取掘金 cookie
-jj_cookie = os.environ["JJ_COOKIE"]
+jj_cookie = os.environ.get("JJ_COOKIE")
 
 # 掘金 api_url
-baseUrl = 'https://api.juejin.cn/'
-checkInUrl = baseUrl + 'growth_api/v1/check_in'
-lotteryUrl = baseUrl + 'growth_api/v1/lottery/draw'
+baseUrl = "https://api.juejin.cn/"
+checkInUrl = baseUrl + "growth_api/v1/check_in"
+lotteryUrl = baseUrl + "growth_api/v1/lottery/draw"
 
 # user-agent
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
+user_agent_list = [
+    "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Mobile Safari/537.36 Edg/118.0.2088.76",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+]
+headers = {
+    "User-Agent": user_agent_list[random.randrange(0, len(user_agent_list))]
+}
+
 
 # server 酱消息推送
 def send_server(title, content):
     url = "https://sctapi.ftqq.com/%s.send" % server_key
-    params = {'text': title, 'desp': content}
+    params = {"text": title, "desp": content}
     resp = requests.post(url, params=params)
-    print('server 酱推送状态码: %s' % resp.status_code)
+    print("server 酱推送状态码: %s" % resp.status_code)
+
 
 # 入口
-if __name__ == '__main__':
-    checkInResp = requests.post(checkInUrl, headers=headers, cookies={'Cookie': jj_cookie})
-    lotteryResp = requests.post(lotteryUrl, headers=headers, cookies={'Cookie': jj_cookie})
-    resultMsg = "掘金签到结果\n" + checkInResp.text + "\n 掘金抽奖结果\n" + lotteryResp.text
-    if server_key:
-        send_server('掘金签到+每日抽奖', resultMsg)
+if __name__ == "__main__":
+    checkInResp = requests.post(
+        checkInUrl, headers=headers, cookies={"Cookie": jj_cookie}
+    )
+    lotteryResp = requests.post(
+        lotteryUrl, headers=headers, cookies={"Cookie": jj_cookie}
+    )
+
+    checkin_result = "未知"
+    dict = json.loads(checkInResp.text)
+    if dict["err_no"] == 0 and dict["data"]:
+        data = dict["data"]
+        checkin_result = "签到成功! 今日新增矿石 %s, 共有矿石 %d" % (
+            data["lottery_name"],
+            data["sum_point"],
+        )
     else:
-        print('未启用 server 酱通知')
-    print('本次签到与抽奖结果信息:\n %s' % resultMsg)
+        checkin_result = "签到失败, %s" % dict["err_msg"]
+
+    lottery_result = "未知"
+    dict = json.loads(lotteryResp.text)
+    if dict["err_no"] == 0 and dict["data"]:
+        data = dict["data"]
+        lottery_result = "抽中%s" % data["lottery_name"]
+    else:
+        lottery_result = "未抽中, %s" % dict["err_msg"]
+
+    resultMsg = "掘金签到结果\n" + checkin_result + "\n 掘金抽奖结果\n" + lottery_result
+    if server_key:
+        send_server("掘金签到+每日抽奖 ", resultMsg)
+    print("本次签到与抽奖结果信息:\n %s" % resultMsg)
